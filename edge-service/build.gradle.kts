c591a2e30791a043c501dd66ca316881526038a2
goss.beta <goss.beta@gmail.com>
2023-12-26 20:13:41 +0900

    feat(edge-service): downgrade spring version

--- edge-service/build.gradle.kts
+++ edge-service/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "2.7.18"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
 }
@@ -18,8 +18,8 @@ repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2023.0.0"
-extra["testcontainersVersion"] = "1.17.3"
+extra["springCloudVersion"] = "2021.0.8"
+extra["testcontainersVersion"] = "1.19.3"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
--- edge-service/src/main/kotlin/com/polarbookshop/edgeservice/config/SecurityConfig.kt
+++ edge-service/src/main/kotlin/com/polarbookshop/edgeservice/config/SecurityConfig.kt
@@ -1,13 +1,23 @@
 package com.polarbookshop.edgeservice.config
 
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository
+import org.springframework.security.web.server.csrf.CsrfToken
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+
 
 @EnableWebFluxSecurity
 class SecurityConfig {
@@ -18,13 +28,35 @@ class SecurityConfig {
     ): SecurityWebFilterChain {
         return http
             .authorizeExchange { exchange ->
-                exchange.anyExchange().authenticated()
+                exchange
+                    .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
+                    .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
+                    .anyExchange().authenticated()
+            }
+            .exceptionHandling { exceptionHandling ->
+                exceptionHandling.authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
             }
             .oauth2Login(Customizer.withDefaults())
             .logout { logout ->
                 logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository))
             }
+            .csrf { csrf ->
+                csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+            }
             .build()
+    }
+
+    @Bean
+    fun csrfWebFilter(): WebFilter {
+        return WebFilter { exchange: ServerWebExchange, chain: WebFilterChain ->
+            exchange.response.beforeCommit {
+                Mono.defer {
+                    val csrfToken: Mono<CsrfToken>? = exchange.getAttribute<Mono<CsrfToken>>(CsrfToken::class.java.name)
+                    csrfToken?.then() ?: Mono.empty()
+                }
+            }
+            chain.filter(exchange)
+        }
     }
 
     private fun oidcLogoutSuccessHandler(
